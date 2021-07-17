384e1fb2b654f3694c8c0d4b864987ba861ae760
Tbread <81889764+Tbread@users.noreply.github.com>
2021-07-17 16:32:12 +0900

    User테이블 id생성전략 변경

--- src/main/java/com/hh18/tenorbackendservice/models/User.java
+++ src/main/java/com/hh18/tenorbackendservice/models/User.java
@@ -26,7 +26,7 @@ public class User extends TimeStamped {
     }
 
     // ID가 자동으로 생성 및 증가합니다.
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
