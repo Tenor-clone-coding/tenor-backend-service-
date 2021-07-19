0bddc88f386387eb1f3e0b23ebdc7a43ee8cd033
Tbread <81889764+Tbread@users.noreply.github.com>
2021-07-19 14:27:06 +0900

    UserInfoController 삭제, UserController에 통합

--- src/main/java/com/hh18/tenorbackendservice/controller/UserController.java
+++ src/main/java/com/hh18/tenorbackendservice/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.hh18.tenorbackendservice.controller;
 
 import com.hh18.tenorbackendservice.dto.SignupRequestDto;
+import com.hh18.tenorbackendservice.dto.UserInfoDto;
+import com.hh18.tenorbackendservice.security.UserDetailsImpl;
 import com.hh18.tenorbackendservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -57,4 +61,18 @@ public class UserController {
 
         return "redirect:/";
     }
+
+    @GetMapping("user/info")
+    @ResponseBody
+    public UserInfoDto getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        if (userDetails == null){
+            return new UserInfoDto(false,"로그인상태가 아닙니다.",null,null,null);
+        } else {
+            return new UserInfoDto(true,"로그인된 사용자입니다.",
+                    userDetails.getUser().getId(),
+                    userDetails.getUser().getEmail(),
+                    userDetails.getUser().getUsername());
+        }
+    }
+
 }
